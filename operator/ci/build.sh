1c5b0ca54b3eec7921991e23b999c8fbd6cdfee9
Wolf Oliver <owolf@anynines.com>
2016-08-31 15:29:25 +0200

    copied ci assets from basics to operator

---
+++ operator/ci/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+pushd basics/site
+  hugo
+popd
+
+pushd basics/slides
+  hugo
+popd
+
+mkdir built
+cp -r basics/site/public/* ../built/
+mkdir -p ../built/slides
+cp -r basics/slides/public/* ../built/slides/
