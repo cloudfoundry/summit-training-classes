f64978a0455063e69d6b7b0e4dbe6c3c1080bf94
norman-abramovitz <norm@starkandwayne.com>
2018-02-25 09:03:43 -0800

    1. Make it slightly easier to deploy in test envirornments   a. Make sure the user actually logs into their cf environment   b. Override organization using the environment variable STC_ORG   c. Override space  using the environment variable STC_SPACE

--- basics/scripts/deploy.sh
+++ basics/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-# Make sure you are logged in before running this script!!!
-cf target -o cfcommunity -s cforg
+STC_ORG=${STC_ORG:-'cfcommunity'}
+STC_SPACE=${STC_ORG:-'cforg'}
+
+cf api | grep "^No[t ]" && exit 1 
+
+cf target -o ${STC_ORG} -s ${STC_SPACE}
 cf push basics-workshop -b staticfile_buildpack -p site/public/ 
